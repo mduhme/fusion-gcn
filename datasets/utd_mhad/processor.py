0c1464bdb257f0af47f30bfd608fb8399d8fbdf0
Michael Duhme <mduhme@uni-koblenz.de>
2020-10-21 11:08:36 +0200

    Commit processor.py before splitting into multiple files

--- datasets/utd_mhad/processor.py
+++ datasets/utd_mhad/processor.py
@@ -22,7 +22,10 @@ class Processor:
         self.structure = None
         self.mode = mode
         self.max_sequence_length = 0
-        self.main_modality = self.get_required_loaders()[0]
+
+    @property
+    def main_modality(self):
+        return self.get_required_loaders()[0]
 
     @property
     def main_structure(self):
@@ -43,10 +46,20 @@ class Processor:
 
         # If sequence length is None, take max sequence length from first structure
         if self.max_sequence_length is None:
-            self.max_sequence_length = self.structure[self.main_modality].max_sequence_length
+            self.max_sequence_length = self.main_structure.max_sequence_length
 
     @abc.abstractmethod
     def get_required_loaders(self) -> Sequence[str]:
+        """
+        Return a list of required loaders (modalities).\n
+        For example, if this function returns only a single modality: ["skeleton"]
+        the function process receives a dictionary of type: {"skeleton": skeleton_value}\n
+        Returning ["skeleton", "rgb"] will receive {"skeleton": skeleton_value, "rgb": rgb_value}\n
+        The first returned modality (in this example "skeleton") will be the main modality
+        and be used for interpolating other requested modalities.
+
+        :return: List of required loaders
+        """
         pass
 
     @abc.abstractmethod
@@ -77,12 +90,13 @@ class Processor:
         """
         samples = self._prepare_samples(copy.copy(samples))
         for modality in samples:
-            samples[modality] = interpolators[modality].interpolate(samples[modality], sample_lengths[modality])
+            samples[modality] = interpolators[modality].interpolate(samples[modality], sample_lengths[modality],
+                                                                    sample_lengths[self.main_modality])
             samples[modality] = self._pad_sequence(samples[modality], modality)
 
         if len(samples) == 1:
             samples = samples[self.main_modality]
-        sample = self._process(samples, **kwargs)
+        sample = self._process(samples, sample_lengths, interpolators, **kwargs)
 
         if writer:
             writer.collect_next(sample)
@@ -90,11 +104,27 @@ class Processor:
         return sample
 
     @abc.abstractmethod
-    def _process(self, sample, **kwargs) -> np.ndarray:
+    def _process(self, sample, sample_lengths: dict, interpolators: Dict[str, SampleInterpolator],
+                 **kwargs) -> np.ndarray:
+        """
+        Process a single sample. 'sample' is a dict if get_required_loaders returns more than a single value.
+        This function is called after interpolation and padding, so all samples are guaranteed to have equal length.
+
+        :param sample: interpolated and padded sample
+        :param sample_lengths: dict of sample lengths before interpolation and padding
+        :param interpolators: interpolators used for each modality
+        :param kwargs: additional mode-specific arguments
+        :return: processed sample
+        """
         pass
 
-    # noinspection PyMethodMayBeStatic
     def _prepare_samples(self, samples: dict) -> dict:
+        """
+        Overridable method that is called before samples are interpolated and padded.
+
+        :param samples: raw samples
+        :return: modified samples (identity by default)
+        """
         return samples
 
     def _pad_sequence(self, sequence, modality: str):
@@ -124,12 +154,26 @@ class MatlabInputProcessor(Processor):
         return NumpyWriter(out_path, self.main_structure.target_type, shape)
 
     def _make_simple_output_shape(self, input_shape: tuple, num_samples: int):
+        """
+        Create output shape equivalent to input shape with number of frames set to self.max_sequence_length.
+
+        :param input_shape: input shape
+        :param num_samples: Total number of samples
+        :return: output shape
+        """
         shape = [num_samples, *input_shape]
         shape[1] = self.max_sequence_length
         return shape
 
     @abc.abstractmethod
-    def _get_output_shape(self, num_samples: int, **kwargs):
+    def _get_output_shape(self, num_samples: int, **kwargs) -> Sequence[int]:
+        """
+        The output shape is used to create one big array where all processed samples will be stored.
+
+        :param num_samples: Total number of samples
+        :param kwargs: additional named arguments
+        :return: output shape
+        """
         pass
 
 
@@ -138,27 +182,42 @@ class SkeletonProcessor(MatlabInputProcessor):
         super().__init__(mode)
 
     def get_required_loaders(self) -> Sequence[str]:
+        if self.mode == "skele+imu":
+            return "skeleton", "inertial"
         return "skeleton",
 
-    def _get_output_shape(self, num_samples: int, **kwargs):
+    def _get_output_shape(self, num_samples: int, **kwargs) -> Sequence[int]:
         # self.loader.input_shape is (num_frames, num_joints[=20], num_channels[=3])
         # shape is (num_samples, num_channels[=3], num_frames, num_joints[=20], num_bodies[=1])
+        num_joints = self.main_structure.input_shape[1]
+
+        if self.mode == "skele+imu":
+            num_joints += 2
+
         return [
             num_samples,
             self.main_structure.input_shape[-1],
             self.max_sequence_length,
-            self.main_structure.input_shape[1],
-            1
+            num_joints,
+            kwargs.get("num_bodies", 1)
         ]
 
-    def _process(self, sample, **kwargs) -> np.ndarray:
+    def _process(self, sample, sample_lengths: dict, interpolators: Dict[str, SampleInterpolator],
+                 **kwargs) -> np.ndarray:
         # MHAD only has actions were one 'body' is involved: add single dimension for body
-        sample = np.expand_dims(sample, axis=0)
+        if sample.ndim == 3:
+            sample = np.expand_dims(sample, axis=0)
         assert skeleton_util.is_valid(sample)
+        # TODO generalize this
         sample = skeleton_util.normalize_skeleton(sample, 2, (3, 2), (4, 8))
         # Permute from (num_bodies, num_frames, num_joints, num_channels)
         # to (num_channels, num_frames, num_joints, num_bodies)
         sample = sample.transpose((3, 1, 2, 0))
+
+        if self.mode == "skele+imu":
+            # Add acc and gyro to normalized skeleton
+            pass
+
         return sample
 
 
@@ -169,7 +228,7 @@ class InertialProcessor(MatlabInputProcessor):
     def get_required_loaders(self) -> Sequence[str]:
         return "inertial",
 
-    def _get_output_shape(self, num_samples: int, **kwargs):
+    def _get_output_shape(self, num_samples: int, **kwargs) -> Sequence[int]:
         if self.mode == "signal_image":
             # Compute signal image
             input_shape = signal_util.get_signal_image_shape(self.max_sequence_length,
@@ -182,7 +241,8 @@ class InertialProcessor(MatlabInputProcessor):
 
         return self._make_simple_output_shape(self.main_structure.input_shape, num_samples)
 
-    def _process(self, sample, **kwargs) -> np.ndarray:
+    def _process(self, sample, sample_lengths: dict, interpolators: Dict[str, SampleInterpolator],
+                 **kwargs) -> np.ndarray:
         if self.mode == "signal_image":
             return signal_util.compute_signal_image(sample, kwargs.get("signal_image_cutoff", False))
         elif self.mode == "signal_image_feature":
@@ -202,10 +262,11 @@ class DepthProcessor(MatlabInputProcessor):
 
         return "depth",
 
-    def _get_output_shape(self, num_samples: int, **kwargs):
+    def _get_output_shape(self, num_samples: int, **kwargs) -> Sequence[int]:
         return self._make_simple_output_shape(self.main_structure.input_shape, num_samples)
 
-    def _process(self, sample, **kwargs) -> np.ndarray:
+    def _process(self, sample, sample_lengths: dict, interpolators: Dict[str, SampleInterpolator],
+                 **kwargs) -> np.ndarray:
         return sample
 
 
@@ -228,11 +289,13 @@ class RGBVideoProcessor(Processor):
     def get_required_loaders(self) -> Sequence[str]:
         if self.mode == "rgb_skeleton_patches":
             return "rgb", "skeleton"
+        elif self.mode == "rgb_openpose_skeleton_patches":
+            return "rgb", "openpose_skeleton", "skeleton"
 
         return "rgb",
 
     def collect(self, out_path: str, num_samples: int, **kwargs) -> FileWriter:
-        if self.mode == "rgb_skeleton_patches":
+        if self.mode in ("rgb_skeleton_patches", "rgb_openpose_skeleton_patches"):
             out_path += ".npy"
             # shape is (num_samples, num_channels[=Output Channels of CNN], num_frames, num_joints[=20], num_bodies[=1])
             shape = [
@@ -240,7 +303,7 @@ class RGBVideoProcessor(Processor):
                 feature_util.get_feature_size(kwargs.get("rgb_feature_model", None)),
                 self.max_sequence_length,
                 self.structure["skeleton"].input_shape[1],  # num joints same as skeleton
-                1
+                kwargs.get("num_bodies", 1)  # num bodies
             ]
             return NumpyWriter(out_path, np.float32, shape)
 
@@ -251,11 +314,54 @@ class RGBVideoProcessor(Processor):
         return writer
 
     def _prepare_samples(self, samples: dict) -> dict:
+        # Incoming samples["rgb"] is cv2.VideoCapture -> create generator over frames
         samples["rgb"] = video_util.frame_iterator(samples["rgb"])
         return samples
 
-    def _process(self, sample, **kwargs):
-        if self.mode == "rgb_skeleton_patches":
-            pass
+    def _process(self, sample, sample_lengths: dict, interpolators: Dict[str, SampleInterpolator], **kwargs):
+        if "skeleton_patches" in self.mode:
+            extractor = kwargs.get("skeleton_patch_extractor", None)
+            if extractor is None:
+                raise RuntimeError(
+                    "RGBVideoProcessor using mode 'skeleton_patches' but no SkeletonPatchExtractor provided")
+
+            # feature CNN model to use: default (None) is resnet18
+            feature_model = kwargs.get("rgb_feature_model", None)
+            out_shape = [
+                kwargs.get("num_bodies", 1),  # num_bodies
+                self.max_sequence_length,  # num_frames
+                self.structure["skeleton"].input_shape[1],  # num_joints
+                feature_util.get_feature_size(feature_model)  # num_channels
+            ]
+            out_sample = np.zeros(out_shape, dtype=self.structure["skeleton"].target_type)
+
+            if self.mode == "rgb_skeleton_patches":
+                raise NotImplementedError()
+            elif self.mode == "rgb_openpose_skeleton_patches":
+                # Move axis:
+                # Shape from (num_frames, num_joints, 2, num_bodies) to (num_bodies, num_frames, num_joints, 2)
+                rgb_coords = np.moveaxis(sample["openpose_skeleton"], -1, 0)
+
+                for frame_idx, frame in enumerate(sample["rgb"]):
+                    for body_idx, sequence in enumerate(rgb_coords):
+                        coords = sequence[frame_idx]
+
+                        # Don't waste processing on 'empty' skeletons (coming from zero-padding)
+                        if not skeleton_util.is_valid(coords):
+                            continue
+
+                        # Extract RGB patches
+                        patches = extractor.get_skeleton_rgb_patches(frame, coords,
+                                                                     kwargs.get("patch_radius", 64), False)
+
+                        # Encode patches using CNN and write to output array
+                        for patch_idx, patch in enumerate(patches):
+                            feature = feature_util.encode_sample(patch, feature_model)
+                            out_sample[body_idx, frame_idx, patch_idx] = feature.astype(out_sample.dtype)
+
+            # Shape from
+            # (num_bodies, num_frames, num_joints, num_channels) to (num_channels, num_frames, num_joints, num_bodies)
+            out_sample_0 = out_sample.swapaxes(0, -1)
+            return out_sample_0
 
         return sample

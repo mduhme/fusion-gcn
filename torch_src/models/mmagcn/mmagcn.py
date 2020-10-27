7b6972b7e9eefeb5293399a7f65de3ab037445c8
Michael Duhme <mduhme@uni-koblenz.de>
2020-10-27 14:04:32 +0100

    mmagcn small update

--- torch_src/models/mmagcn/mmagcn.py
+++ torch_src/models/mmagcn/mmagcn.py
@@ -1,7 +1,9 @@
 import math
+import torch
 import torch.nn as nn
 
 import models.mmagcn.agcn as agcn
+import models.mmagcn.resnet2p1d as r2p1d
 
 
 # noinspection PyAbstractClass
@@ -26,25 +28,27 @@ class SkeletonImuCombined(nn.Module):
 class SkeletonImuCombinedRgbPatches(nn.Module):
     def __init__(self, data_shape, num_classes: int, graph, **kwargs):
         super().__init__()
-        num_layers = kwargs.get("num_layers", 6)
+        num_layers = kwargs.get("num_layers", 10)
 
         # create new edges for imu data, append them to center joint
         skeleton_imu_graph = graph.with_new_edges([
             (graph.num_vertices, graph.center_joint),
             (graph.num_vertices + 1, graph.center_joint)
         ])
-        self._skeleton_model = agcn.Model(data_shape["skeleton"], num_classes, skeleton_imu_graph,
-                                          num_layers=10, without_fc=False)
-        # self._rgb_model = agcn.Model(data_shape["rgb"], num_classes, graph, num_layers=num_layers,
-        #                              start_feature_size=512, without_fc=True)
-        # self._fc = nn.Linear(self.layers[-1].out_channels, num_classes)
+        # self._skeleton_model = agcn.Model(data_shape["skeleton"], num_classes, skeleton_imu_graph,
+        #                                   num_layers=num_layers, without_fc=True)
+        self._rgb_model = agcn.Model(data_shape["rgb"], num_classes, graph,
+                                     num_layers=num_layers, start_feature_size=512, without_fc=False)
+        # self.fc = nn.Linear(self._skeleton_model.out_channels + self._rgb_model.out_channels, num_classes)
         # nn.init.normal_(self.fc.weight, 0, math.sqrt(2. / num_classes))
 
     def forward(self, x):
         skeleton_x, rgb_x = x
-        skeleton_x = self._skeleton_model(skeleton_x)
-        # rgb_x = self._rgb_model(rgb_x)
-        return skeleton_x
+        # skeleton_x = self._skeleton_model(skeleton_x)
+        rgb_x = self._rgb_model(rgb_x)
+        # x = torch.cat((skeleton_x, rgb_x), 1)
+        # x = self.fc(x)
+        return rgb_x
 
 
 # noinspection PyAbstractClass

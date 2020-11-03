- bugfix: when loading more than one modality for training: feature should be saved in a dict, not a list - bugfix: missing 'return' in mixed precision step function

torch_src/session/procedures/step.py
@@ -50,7 +50,7 @@ class MixedPrecisionStep(Step):
     def forward(self, model: torch.nn.Module, loss_function: torch.nn.Module, features: torch.Tensor,
                 label: torch.Tensor, loss_quotient: int = 1, **kwargs):
         with autocast():
-            self._inner.forward(model, loss_function, features, label, loss_quotient, **kwargs)
+            return self._inner.forward(model, loss_function, features, label, loss_quotient, **kwargs)
 
     def backward(self, loss: torch.Tensor, **kwargs):
         self._loss_scale.scale(loss).backward()

torch_src/session/session.py
@@ -161,8 +161,8 @@ class Session:
 
         for features_batch, label_batch, indices in dataset:
             with torch.no_grad():
-                if type(features_batch) is list:
-                    features = list(map(lambda x: x.float().cuda(), features_batch))
+                if type(features_batch) is dict:
+                    features = {k: v.float().cuda() for k, v in features_batch.items()}
                 else:
                     features = features_batch.float().cuda()
                 label = label_batch.long().cuda()
@@ -186,8 +186,8 @@ class Session:
         model.eval()
         with torch.no_grad():
             for features_batch, label_batch, indices in dataset:
-                if type(features_batch) is list:
-                    features = list(map(lambda x: x.float().cuda(), features_batch))
+                if type(features_batch) is dict:
+                    features = {k: v.float().cuda() for k, v in features_batch.items()}
                 else:
                     features = features_batch.float().cuda()
                 label = label_batch.long().cuda()